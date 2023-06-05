9395f98765759d9c3a9ed03db67a0425ca00cb09
AntiqueRised <imjoodie@gmail.com>
2023-06-04 20:45:53 -0500

    added majority of functionality, not fully complete

--- db/functions.js
+++ db/functions.js
@@ -1,38 +1,41 @@
 const db = require('./connection');
 
-
-
-
 function viewAllEmployees(callback) {
-db.promise().query(
-    `SELECT 
-        employee.id, 
-        employee.first_name, 
-        employee.last_name, 
-        role.title, 
-        role.salary, 
-        department.name, 
-        CONCAT(manager.first_name, " ", manager.last_name) AS manager 
-    FROM 
-        employee 
-    INNER JOIN 
-        role on employee.role_id = role.id 
-    INNER JOIN 
-        department on role.department_id = department.id 
-    LEFT JOIN 
-        employee manager on manager.id = employee.manager_id;`
-    )
-    .then(([rows, fields]) => {
-        console.table(rows)
-        callback()})
-      .catch(console.table);
+    db.promise().query(
+        `SELECT 
+            employee.id AS ID, 
+            employee.first_name AS First_Name, 
+            employee.last_name AS Last_Name, 
+            role.title AS Position, 
+            role.salary AS Salary, 
+            department.name AS Department, 
+            CONCAT(manager.first_name, " ", manager.last_name) AS Manager 
+        FROM 
+            employee 
+        INNER JOIN 
+            role on employee.role_id = role.id 
+        INNER JOIN 
+            department on role.department_id = department.id 
+        LEFT JOIN 
+            employee manager on manager.id = employee.manager_id;`
+        )
+        .then(([rows, fields]) => {
+            console.table(rows)
+            callback()})
+        .catch(console.table);
 };
 
-function addEmployee() {
+async function addEmployee(addEmployeeRole, addEmployeeManager, addFirstName, addLastName, callback) {
+    const [roleRow] = await db.promise().query(`SELECT id FROM role WHERE title = '${addEmployeeRole}'`)
+    const [managerRow] = await db.promise().query(`SELECT id FROM employee WHERE CONCAT(first_name, ' ', last_name) LIKE '${addEmployeeManager}'`)
+    const role_id = roleRow[0].id;
+    const manager_id = managerRow[0].id;
     db.promise().query(
-
-    )
-}
+        `INSERT INTO
+            employee
+        VALUES
+            (default, '${addFirstName}', '${addLastName}', ${role_id}, ${manager_id} )`).then(() => callback())
+};
 
 function updateEmployeeRole() {
     db.promise().query(
@@ -40,13 +43,13 @@ function updateEmployeeRole() {
     )
 }
 
-function viewRoles() {
+function viewRoles(callback) {
     db.promise().query(
     `SELECT
-        role.id,
-        role.title,
-        role.salary,
-        department.name AS department
+        role.id AS ID,
+        role.title AS Title,
+        department.name AS Department,
+        role.salary AS Salary
     FROM
         role
     INNER JOIN
@@ -65,12 +68,14 @@ function addRole() {
     )
 }
 
-function viewDepartments() {
+function viewDepartments(callback) {
     db.promise().query(
     `SELECT
-        department.id,
-        department.title,
-    `        
+        department.id AS ID,
+        department.name AS Department
+    FROM
+        department
+    `    
     )
     .then(([rows, fields]) => {
         console.table(rows)
@@ -84,4 +89,47 @@ function addDepartment() {
     )
 }
 
-module.exports = { viewAllEmployees, addEmployee, viewRoles, viewDepartments}
+//inquirer prompt functions
+
+async function rolesCLI() {
+    return db.promise().query(
+    `SELECT
+        title
+    FROM
+        role
+    `    
+    )
+    .then(([rows, fields]) => {
+      return rows.map(row => row.title)})
+    .catch(console.log);
+};
+
+async function deptsCLI() {
+    return db.promise().query(
+    `SELECT
+        department.name
+    FROM
+        department
+    `    
+    )
+    .then(([rows, fields]) => {
+      return rows.map(row => row.name)})
+    .catch(console.log);
+}
+
+async function managersCLI() {
+    return db.promise().query(
+    `SELECT
+        CONCAT(first_name, " ", last_name) AS full_name 
+    FROM 
+        employee
+    WHERE 
+        manager_id IS NULL
+    `    
+    )
+    .then(([rows, fields]) => {
+      return rows.map(row => row.full_name)})
+    .catch(console.log);
+};
+
+module.exports = { viewAllEmployees, addEmployee, viewRoles, viewDepartments, rolesCLI, deptsCLI, managersCLI}
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
 // Node Packages
 const inquirer = require('inquirer');
 const functions = require('./db/functions');
+
 
 const questions = [
   {
@@ -10,23 +12,55 @@ const questions = [
     choices: [
       'View All Employees',
       'Add Employee',
-      'Update Employee Role',
+      // 'Update Employee Role',
       'View All Roles',
-      'Add Role',
+      // 'Add Role',
       'View All Departments',
-      'Add Department',
+      // 'Add Department',
       'Quit'
     ]
   }
 ];
 
+
 function init() {
-  inquirer.prompt(questions).then((answers) => {
-    const choice = answers.menu; // Retrieve the selected menu choice
+  inquirer.prompt(questions).then((input) => {
+    const choice = input.menu; // Retrieve the selected menu choice
     
     switch (choice) {
         case 'View All Employees':
             functions.viewAllEmployees(init); // Call the viewEmployees function from the functions module
+            break;
+        case 'Add Employee':
+              async function rolesPrompt() {
+                let roles = await functions.rolesCLI()
+                let managerList = await functions.managersCLI()
+              inquirer.prompt([
+                {
+                  type: 'input',
+                  name: 'addFirstName',
+                  message: 'What is the first name of the new employee?'
+                },
+                {
+                  type: 'input',
+                  name: 'addLastName',
+                  message: 'What is the last name of the new employee?'
+                },
+                {
+                  type: 'list',
+                  name: 'addEmployeeRole',
+                  message: `What is the employee's role:`,
+                  choices: roles
+                },
+                {
+                  type: 'list',
+                  name: 'addEmployeeManager',
+                  message: `Who will be the new employee's Manager?`,
+                  choices: managerList
+                }]).then((input) => {
+                functions.addEmployee(input.addEmployeeRole, input.addEmployeeManager, input.addFirstName, input.addLastName, init)
+              })};
+              rolesPrompt();
             break;
         case 'View All Departments':
             functions.viewDepartments(init);
